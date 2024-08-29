7a8d1abef84ac1a1b7803a33017a9cc4d584740e
Badal Aryal <badal.aryal@gmail.com>
2024-08-29 23:09:10 +0545

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Badal  Aryal on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
